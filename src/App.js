71ae0315acbd602d67412550f8af3e59baa7f152
bibek190 <bibek9818917036@gmail.com>
2024-01-11 15:14:22 +1100

    navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World its Bibek Back again.</h1>
+    <div>
+      <Navbar />
     </div>
   );
 }
