a1198b5e2bddeb3dc1cb9a2aa62c872fcd2e7bc0
bibek190 <bibek9818917036@gmail.com>
2024-01-18 14:17:50 +1100

    hero section home page

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import logo from "../Assets/logo.png";
 import cart_icon from "../Assets/cart_icon.png";
+import { Link } from "react-router-dom";
 const Navbar = () => {
+  const [menu, setMenu] = useState("shop");
+
   return (
     <div className="navbar">
       <div className="nav-logo">
@@ -10,17 +13,56 @@ const Navbar = () => {
         <p>Shopper</p>
       </div>
       <ul className="nav-menu">
-        <li>
-          Shop
-          <hr />
+        <li
+          onClick={() => {
+            setMenu("shop");
+          }}
+        >
+          <Link style={{ textDecoration: "none" }} to="/">
+            Shop
+          </Link>
+          {menu === "shop" ? <hr /> : <></>}
         </li>
-        <li>Men</li>
-        <li>Women</li>
-        <li>kids</li>
+        <li
+          onClick={() => {
+            setMenu("mens");
+          }}
+        >
+          <Link style={{ textDecoration: "none" }} to="/mens">
+            Men
+          </Link>{" "}
+          {menu === "mens" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("women");
+          }}
+        >
+          <Link style={{ textDecoration: "none" }} to="/women">
+            Women
+          </Link>{" "}
+          {menu === "women" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("kids");
+          }}
+        >
+          <Link style={{ textDecoration: "none" }} to="/kids">
+            Kids
+          </Link>{" "}
+          {menu === "kids" ? <hr /> : <></>}
+        </li>
       </ul>
       <div className="nav-login-cart">
-        <button>Login</button>
-        <img src={cart_icon} alt="" />
+        <button>
+          <Link style={{ textDecoration: "none" }} to="/login">
+            Login
+          </Link>
+        </button>
+        <Link to="/cart">
+          <img src={cart_icon} alt="" />
+        </Link>
         <div className="nav-cart-count">0</div>
       </div>
     </div>
