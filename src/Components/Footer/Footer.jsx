00ce7b0bd839bfbe456f0a49eeb3c429297b3f87
bibek190 <bibek9818917036@gmail.com>
2024-01-24 15:23:35 +1100

    complete home page

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import "./Footer.css";
 import footer_logo from "../Assets/logo_big.png";
+import instagram_icon from "../Assets/instagram_icon.png";
+import pintrest_icon from "../Assets/pintester_icon.png";
+import whatsapp_icon from "../Assets/whatsapp_icon.png";
 
 const Footer = () => {
   return (
@@ -16,6 +19,21 @@ const Footer = () => {
         <li>About</li>
         <li>Contact</li>
       </ul>
+      <div className="footer-social-icon">
+        <div className="footer-icons-container">
+          <img src={instagram_icon} alt="" />
+        </div>
+        <div className="footer-icons-container">
+          <img src={pintrest_icon} alt="" />
+        </div>
+        <div className="footer-icons-container">
+          <img src={whatsapp_icon} alt="" />
+        </div>
+      </div>
+      <div className="footer-copyright">
+        <hr />
+        <p>Copyright @ 2023 - all right reserved</p>
+      </div>
     </div>
   );
 };
--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -14,7 +14,6 @@ const Shop = () => {
       <Offer />
       <NewCollections />
       <NewsLetter />
-      <Footer />
     </div>
   );
 };
