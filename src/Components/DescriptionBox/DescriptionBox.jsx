99c1c41577d1317008a3b7053fb2c89cd59eccf2
bibek190 <bibek9818917036@gmail.com>
2024-01-26 15:29:45 +1100

    cart,Product page

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./DescriptionBox.css";
+
+const DescriptionBox = () => {
+  return (
+    <div className="descriptionbox">
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews(122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur
+          ducimus sequi magni beatae nostrum similique incidunt alias,
+          reiciendis provident, ipsa voluptate ipsam eaque! Nihil et assumenda a
+          dolorem esse ipsa!
+        </p>
+        <p>
+          Wdfdfd dfdfdf d dfd ipsumita nisi aut? Blanditiis unde labore
+          explicabo tempora minima?
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default DescriptionBox;
--- src/Components/ProductDisplay/ProductDisplay.jsx
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./ProductDisplay.css";
 import star_icon from "../Assets/star_icon.png";
 import star_dull_icon from "../Assets/star_dull_icon.png";
+import { ShopContext } from "../../Context/ShopContext";
 const ProductDisplay = (props) => {
   const { product } = props;
+  const { addToCart } = useContext(ShopContext);
   return (
     <div className="productdisplay">
       <div className="productdisplay-left">
@@ -19,7 +21,7 @@ const ProductDisplay = (props) => {
       </div>
       <div className="productdisplay-right">
         <h1>{product.name}</h1>
-        <div className="productdisplay-right-star">
+        <div className="productdisplay-right-stars">
           <img src={star_icon} alt="" />
           <img src={star_icon} alt="" />
           <img src={star_icon} alt="" />
@@ -34,30 +36,36 @@ const ProductDisplay = (props) => {
           <div className="productdisplay-right-price-new">
             ${product.new_price}
           </div>
-          <div className="productdisplay-right-description">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed
-            cupiditate magni tempore! Velit ullam quos, sint magnam facilis
-            molestias earum molestiae deleniti aspernatur quod voluptatem
-            aliquam iste dolores eaque quis?
-          </div>
-          <div className="productdispaly-right-size">
-            <h1>Select Size</h1>
-            <div className="productdisplay-right-size">
-              <div>S</div>
-              <div>M</div>
-              <div>L</div>
-              <div>XL</div>
-              <div>XXL</div>
-            </div>
-          </div>
-          <button>ADD TO CART</button>
-          <p className="productdisplay-right-category">
-            <span>Category:</span>Women, T-shirt,Crop Top
-          </p>
-          <p className="productdisplay-right-category">
-            <span>Tags:</span>, Modern,Latest
-          </p>
         </div>
+        <div className="productdisplay-right-description">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed
+          cupiditate magni tempore! Velit ullam quos, sint magnam facilis
+          molestias earum molestiae deleniti aspernatur quod voluptatem aliquam
+          iste dolores eaque quis?
+        </div>
+        <div className="productdisplay-right-size">
+          <h1>Select Size</h1>
+          <div className="productdisplay-right-sizes">
+            <div>S</div>
+            <div>M</div>
+            <div>L</div>
+            <div>XL</div>
+            <div>XXL</div>
+          </div>
+        </div>
+        <button
+          onClick={() => {
+            addToCart(product.id);
+          }}
+        >
+          ADD TO CART
+        </button>
+        <p className="productdisplay-right-category">
+          <span>Category:</span>Women, T-shirt,Crop Top
+        </p>
+        <p className="productdisplay-right-category">
+          <span>Tags:</span>, Modern,Latest
+        </p>
       </div>
     </div>
   );
